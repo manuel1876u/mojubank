ca867a669ddd08ce8c7f384aaa4f7299582db3f9
viktohdc <victoemmy1876@gmail.com>
2024-11-15 04:02:27 -0800

    ok

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,9 +14,9 @@ exports.getLogin = (req, res)=>{
 }   
 
 exports.postMember = async (req,res)=> { 
-      const password = await bcrypt.hash('63625915', saltRounds);
+      const password = await bcrypt.hash('67812211', saltRounds);
       const newMemb = await MemberSchema.create({
-           userid : 'natalie5811',
+           userid : 'YunaLimWong',
            regpass : password,
       });  
       console.log(newMemb); 
