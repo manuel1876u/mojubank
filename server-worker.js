14dec67b9a8e1135d19c0e362db224302005af54
viktohdc <victoemmy1876@gmail.com>
2024-11-17 12:23:32 -0800

    oko

---
+++ server-worker.js
@@ -0,0 +1,25 @@
+ 
+  
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('pwa-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/IB/Welcome',
+          '/style.css', 
+          '/styleb.css', 
+          '/script.js',
+          '/images/singerpaulfav.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
